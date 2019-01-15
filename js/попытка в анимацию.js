da8efe88a99fd0bec19570473af971173a3613e5
MarkMartynov1 <45040897+MarkMartynov1@users.noreply.github.com>
2019-01-15 23:00:30 +0300

    Вторая попытка.2

--- js/попытка в анимацию.js
+++
@@ -1,9 +0,0 @@
-var btn = document.querySelector(".hide-button");
-var blockHidden = document.querySelector(".search");
-
-function(evt) {evt.preventDefault(); function showBlock()
-
-  blockHidden.classList.add('b-show');
-
-}
-btn.addEventListener('click', showBlock);
